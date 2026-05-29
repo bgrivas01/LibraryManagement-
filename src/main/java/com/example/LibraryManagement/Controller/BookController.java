44883ab97c8e8b4b49793c4b3dc9c850851741aa
Brayan Rivas <Bgrivas16@gmail.com>
2026-05-29 11:20:23 -0500

    Added BookController with working endpoints

--- src/main/java/com/example/LibraryManagement/Controller/BookController.java
+++ src/main/java/com/example/LibraryManagement/Controller/BookController.java
@@ -1,16 +1,54 @@
 package com.example.LibraryManagement.Controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.LibraryManagement.Service.BookService;
+import com.example.LibraryManagement.entities.Book;
 
 import lombok.RequiredArgsConstructor;
 
+
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/books")
 public class BookController {
 
     private final BookService bookService;
     
+    @GetMapping("")
+    public List<Book> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Book> getBookById(@PathVariable Long id){
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping("")
+    public Book addBook(@RequestBody Book book){
+        return bookService.addBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book){
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id){
+        bookService.deleteBook(id); 
+    }
     
+
 }
